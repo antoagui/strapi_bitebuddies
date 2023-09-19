67bac61fb2aa69760d0c1c8c64988b5e7dbefd41
AleAgu200 <alessandrovasquez1234@gmail.com>
2023-09-19 15:10:53 -0600

    Probando el server

---
+++ src/api/review-local/routes/review-local.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * review-local router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::review-local.review-local');
